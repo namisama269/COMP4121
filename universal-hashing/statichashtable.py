e1fc6dc115f8f2b624652da4feb242e763751e62
Nam Anh Dang <namwarlockclash@gmail.com>
2021-09-28 20:45:44 +1000

    static hash table sort of works

--- universal-hashing/statichashtable.py
+++ universal-hashing/statichashtable.py
@@ -5,6 +5,8 @@
 import math
 from universalhash import isPrime, randVec, universalHash
 
+search  =5
+
 class StaticHashTable:
     def __init__(self, hi):
         self.hi = hi
@@ -37,19 +39,21 @@ class StaticHashTable:
         """
         # check if sum(n_i^2) < 4n for i from 1 to M
         ni = 5*self.hi
+        itt = 1
         while ni >= 4*self.hi:
+            if itt > 1:
+                self.primaryHash = self.randHashFunc()
             ni = 0
             self.fillPrimaryTable()
             for i in range(self.size):
                 ni += len(self.primaryTable[i]) ** 2
-            self.hash1 = self.randHashFunc()
+            itt += 1
 
     def fillPrimaryTable(self):
         for i in range(self.size):
             self.primaryTable[i].clear()
         m = self.size
         r = math.floor(math.log(self.hi, m))
-        print(self.hash1)
         for i in range(1, self.hi+1):
             self.primaryTable[universalHash(i, m, self.hi, self.primaryHash)].append(i)
 
@@ -59,17 +63,17 @@ class StaticHashTable:
 
     def fillSecondaryTable(self, ix):
         tix = self.primaryTable[ix]
-        print(len(tix))
         m = self.findTableSize(len(tix))
-        print(m)
-        print()
         if m is None:
             self.hashTable[ix] = None
             return
         self.hashTable[ix] = [None] * m
         for i in range(len(tix)):
             # j_x = h_ix(x)
-            jx = universalHash(tix[i], m, len(tix), self.secondaryHash[ix])
+            jx = universalHash(tix[i], m, self.hi, self.secondaryHash[ix])
+            if tix[i] == search:
+                print(f"{jx} = hash(({tix[i]}, {m}), {self.hi}, {self.secondaryHash[ix]})")
+                print()
             if self.hashTable[ix][jx] is None:
                 self.hashTable[ix][jx] = tix[i]
             else:
@@ -79,8 +83,18 @@ class StaticHashTable:
 
 
 if __name__ == "__main__":
-    h = StaticHashTable(30)
+    h = StaticHashTable(10)
+    print(h.primaryTable)
     print(h.hashTable)
     print(h.size)
+    n = universalHash(search, h.size, h.hi, h.primaryHash)
+    print(n)
+    n = universalHash(search, h.size, h.hi, h.primaryHash)
+    print(n)
+    n2 = universalHash(search, 2, h.hi, h.secondaryHash[n])
+    print(n2)
+    n2 = universalHash(search, 2, h.hi, h.secondaryHash[n])
+    print(f"{n2} = hash(({search}, {2}), {h.hi}, {h.secondaryHash[n]})")
+    print(n2)
 
         
